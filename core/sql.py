150ca2d681483869c2b035e6d28907887ba211c9
Axel <axel.mesnard@kedgebs.com>
2021-01-30 09:54:37 +0100

    corrected sql + main creation

--- core/sql.py
+++ core/sql.py
@@ -1,18 +1,22 @@
 import mysql.connector
 
 class Sql:
+    def __init__(self):
+        self.connection()
+
     def connection(self):
         self.mydb = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
-            password = ''
+            password = 'azqsazqs282',
+            auth_plugin = 'mysql_native_password'
         )
 
     def dbinit(self):
         self.mycursor = self.mydb.cursor()
-        self.mycursor.execute('CREATE DATABASE openfoodfacts2')
-
+        self.mycursor.execute('CREATE DATABASE IF NOT EXISTS openfoodfacts2')
         sql_file = open('script-db/create_db.sql')
         sql_as_string = sql_file.read()
-        self.mycursor.execute(sql_as_string)
+        self.mycursor.execute('USE openfoodfacts2')
+        self.mycursor.execute(sql_as_string, multi=True)
 
---
+++ main.py
@@ -0,0 +1,6 @@
+from core.sql import Sql
+from core.sql import Sql
+
+connection = Sql()
+
+connection.dbinit()
