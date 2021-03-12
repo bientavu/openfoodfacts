e1dce790359701273a7d4062e61c1a143dac5c43
Axel <axel.mesnard@kedgebs.com>
2021-03-12 16:33:42 +0100

    db_config module creation

---
+++ db_config.py
@@ -0,0 +1,8 @@
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = 'azqsazqs282'
+PORT = 3306
+AUTH_PLUGIN = 'mysql_native_password'
+DATABASE = 'openfoodfacts4'
+CHARSET = 'utf8mb4'
+COLLATION = 'utf8mb4_unicode_ci'
