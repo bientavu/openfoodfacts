e06d677897bbca781745550537aa0bc3716db58b
Axel <axel.mesnard@kedgebs.com>
2021-03-12 16:32:07 +0100

    db config corrections

--- core/database.py
+++ core/database.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import db_config
 
 class Sql:
     def __init__(self):
@@ -8,20 +9,29 @@ class Sql:
         """
         Connection to SQL via localhost root user
         """
-        self.connection = mysql.connector.connect(option_files='db_configuration.conf')
+        self.connection = mysql.connector.connect(
+            host = db_config.HOST,
+            user = db_config.USER,
+            password = db_config.PASSWORD,
+            port = db_config.PORT,
+            auth_plugin = db_config.AUTH_PLUGIN,
+            database = db_config.DATABASE,
+            charset = db_config.CHARSET,
+            collation = db_config.COLLATION
+        )
         mycursor = self.connection.cursor()
-        mycursor.execute('USE openfoodfacts2')
+        mycursor.execute(f'USE {db_config.DATABASE}')
 
     def dbinit(self):
         """
         Database creation + Tables creation via .sql file
         """
         mycursor = self.connection.cursor()
-        mycursor.execute('CREATE DATABASE IF NOT EXISTS openfoodfacts2')
+        # mycursor.execute(f'CREATE DATABASE {db_config.DATABASE} CHARACTER SET {db_config.CHARSET} COLLATE {db_config.COLLATION}')
         sql_file = open('script-db/create_db.sql')
-        sql_as_string = sql_file.read()
-        mycursor.execute('USE openfoodfacts2')
-        for result in mycursor.execute(sql_as_string, multi=True):
-            pass
+        sql_as_string = sql_file.read().split(";")
+        mycursor.execute('USE openfoodfacts4')
+        for sql in sql_as_string:
+            mycursor.execute(sql)
         self.connection.commit()
         mycursor.close()
