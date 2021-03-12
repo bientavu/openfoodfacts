bdf784ecb8c09a022e2214ba2a504c3cf376f887
Axel <axel.mesnard@kedgebs.com>
2021-03-12 16:34:24 +0100

    small update

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from pprint import pprint
 from core.database import Sql
 from core.models import Category, Product
 from core.openfoodfacts import ProductDownloader, ProductCleaner
-from core.program import WelcomeMenu
+from core.view import View
+from core.controller import Controller
 
 # hello = WelcomeMenu()
-# say_hello = hello.Hello()
+# say_hello = hello.hello()
+
+view = View()
+product = Product
+
+program_controller = Controller(view, product)
+program_controller.run()
 
 # create_database = Sql()
 # create_database.dbinit()
 
 # Category.objects.insert_category()
 
-products = ProductDownloader()
-products_dict = products.get_products_info()
+# products = ProductDownloader()
+# products_dict = products.get_products_info()
 
-cleaner = ProductCleaner()
-cleaned_products = cleaner.clean(products_dict)
+# cleaner = ProductCleaner()
+# cleaned_products = cleaner.clean(products_dict)
 
-Product.objects.insert_product(cleaned_products)
+# Product.objects.insert_product(cleaned_products)
