1053fd25f6c1d86d185dcd86e2a88d286f2b5c7c
Axel <axel.mesnard@kedgebs.com>
2021-03-18 17:01:50 +0100

    added choosefood validator method

--- core/input_validators.py
+++ core/input_validators.py
@@ -1,21 +1,32 @@
 
 
 def is_valid_welcome_response(response):
-        """
-        Cheking if the welcome input is True or False to continue
-        Otherwise the welcome message keeps repeating
-        """
-        if response in ['1', '2', '3']:
-            return True
-        else:
-            return False
+    """
+    Cheking if the welcome input is True or False to continue
+    Otherwise the welcome message keeps repeating
+    """
+    if response in ['1', '2', '3']:
+        return True
+    else:
+        return False
 
 def is_valid_category_response(response, categories):
-        """
-        Cheking if the category input is True or False to continue
-        Otherwise the welcome message keeps repeating
-        """
-        if response in [str(number) for number in range(1, len(categories) + 3)]:
-            return True
-        else:
-            return False
+    """
+    Cheking if the category input is True or False to continue
+    Otherwise the welcome message keeps repeating
+    """
+    if response in [str(number) for number in range(1, len(categories) + 3)]:
+        return True
+    else:
+        return False
+
+def is_valid_product_response(response, products):
+
+    """
+    Cheking if the category input is True or False to continue
+    Otherwise the welcome message keeps repeating
+    """
+    if response in [str(number) for number in range(1, len(products) + 3)]:
+        return True
+    else:
+        return False     
