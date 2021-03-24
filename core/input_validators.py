d1209bfb59572de7db2a2059f485faa5fa1195ce
Axel <axel.mesnard@kedgebs.com>
2021-03-24 10:18:30 +0100

    is_valid_suggested_product_response method added + minor corrections

--- core/input_validators.py
+++ core/input_validators.py
@@ -13,7 +13,7 @@ def is_valid_welcome_response(response):
 def is_valid_category_response(response, categories):
     """
     Cheking if the category input is True or False to continue
-    Otherwise the welcome message keeps repeating
+    Otherwise the category message keeps repeating
     """
     if response in [str(number) for number in range(1, len(categories) + 3)]:
         return True
@@ -21,12 +21,21 @@ def is_valid_category_response(response, categories):
         return False
 
 def is_valid_product_response(response, products):
-
     """
-    Cheking if the category input is True or False to continue
-    Otherwise the welcome message keeps repeating
+    Cheking if the product input is True or False to continue
+    Otherwise the product message keeps repeating
     """
     if response in [str(number) for number in range(1, len(products) + 3)]:
         return True
     else:
-        return False     
+        return False
+
+def is_valid_suggested_product_response(response, product):
+    """
+    Cheking if the input is True or False to continue
+    Otherwise the the message keeps repeating
+    """
+    if response in [str(number) for number in range(1, len(product) + 3)]:
+        return True
+    else:
+        return False   
