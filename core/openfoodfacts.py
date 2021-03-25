1ca1a1f197d239a51f81501a7d0e25a79c363580
Axel <axel.mesnard@kedgebs.com>
2021-03-25 16:33:39 +0100

    minor changes

--- core/openfoodfacts.py
+++ core/openfoodfacts.py
@@ -3,8 +3,14 @@ from pprint import pprint
 from .constants import CATEGORIES
 
 class ProductDownloader:
-
+    """
+    Download all the informations of the products.
+    Categories are setup in the module constant.py
+    """
     def get_products_info(self):
+        """
+        Gets all the products informations and put in a list
+        """
         url = "https://fr.openfoodfacts.org/cgi/search.pl"
         number_of_pages = 2
         page_size = 100
@@ -38,17 +44,27 @@ class ProductDownloader:
 
 
 class ProductCleaner:
-
+    """
+    Cleans the list that has been downloaded by
+    the method get_products_info
+    """
     def remove_empty_values(self, product):
+        """
+        Removes all the empty values from the list
+        """
         res = {key: val for key, val in product.items() if val}
         return res
 
     def clean(self, products):
+        """
+        Add all the cleaned products to a new list only if
+        lenght is 5, name length under 100 and store name under 45
+        """
         cleaned_products = []
 
         for product in products:
-            product = self.remove_empty_values(product) # product est passé en argument de store_clean()
+            product = self.remove_empty_values(product)
             if len(product) == 5 and len(product['product_name']) < 100 and len(product['stores']) < 45:
-                cleaned_products.append(product) # le produit n'est ajouté que s'il est valide et après avoir été nettoyé
+                cleaned_products.append(product)
                 
         return cleaned_products
