398142515a1e8bf0a42de80c61c2445852449d36
Axel <axel.mesnard@kedgebs.com>
2021-04-02 10:44:00 +0200

    minor change

--- db_config.py
+++ db_config.py
@@ -3,6 +3,6 @@ USER = 'root'
 PASSWORD = 'azqsazqs282'
 PORT = 3306
 AUTH_PLUGIN = 'mysql_native_password'
-DATABASE = 'openfoodfacts4'
+DATABASE = 'openfoodfacts'
 CHARSET = 'utf8mb4'
 COLLATION = 'utf8mb4_unicode_ci'
