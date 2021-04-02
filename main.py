1b7460cb131cb9f0941c0b82f869b5b14037a51a
Axel <axel.mesnard@kedgebs.com>
2021-04-02 10:31:10 +0200

    pip env + main updated

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-from pprint import pprint
-from core.database import Sql
-from core.models import Category, Product, Substitute
-from core.openfoodfacts import ProductDownloader, ProductCleaner
+from core.models import Product
 from core.views import View
 from core.controllers import Controller
 
-# hello = WelcomeMenu()
-# say_hello = hello.hello()
+def main():
+    """
+    Run the program
+    """
+    view = View()
+    product = Product
 
-view = View()
-product = Product
+    program_controller = Controller(view, product)
+    program_controller.run()
 
-program_controller = Controller(view, product)
-program_controller.run()
-
-# create_database = Sql()
-# create_database.dbinit()
-
-# Category.objects.insert_categories()
-
-# products = ProductDownloader()
-# products_dict = products.get_products_info()
-
-# cleaner = ProductCleaner()
-# cleaned_products = cleaner.clean(products_dict)
-
-# Product.objects.insert_product(cleaned_products)
-
+if __name__ == "__main__":
+    main()
